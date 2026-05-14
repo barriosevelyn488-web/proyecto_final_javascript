d7694f76fc1a0a9b24c6fce3f0d3dae2b16518b6
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-05-14 09:48:30 -0600

    feat: :sparkles: create final login fuction
    fuction for password
    
    by Evelyn

--- FUNCIONESJS/usuario.js
+++ FUNCIONESJS/usuario.js
@@ -1,3 +1,30 @@
 //aqui van las validaciones y perfil
 
 
+    const $submit = document.getElementById("submit"),
+            $password = document.getElementById("password"),
+            $username = document.getElementById("username"),
+            $visible = document.getElementById("visible");
+            $error = document.getElementById("mensaje-error");
+
+    document.addEventListener("change", (e)=>{
+        if(e.target === $visible){
+            if($visible.checked === false) $password.type = "password";
+            else $password.type = "text"
+        }
+    });
+
+    document.addEventListener("click", (e)=>{
+        if(e.target === $submit){
+            e.preventDefault();
+            if($password.value === "ABC123" && $username.value === "Admin@campus.com"){
+                $error.style.display = "none";
+                window.location.href = "index.html";
+            }
+        } else{
+            $error.style.display = "block";
+            $password.value = "";
+        }
+    })
+
+
